4212f1d8a6369d7f66edaa11b9fa6ce4fb077bde
Josh Nguyen <Joshpnguyen@gmail.com>
2021-07-31 22:16:26 -0700

    Adding form to new journal entry page

--- lib/src/add_entry.dart
+++ lib/src/add_entry.dart
@@ -1,25 +1,101 @@
 import 'package:dart_journal/src_export.dart';
 import 'package:flutter/material.dart';
 
-class AddEntry extends StatelessWidget {
+class AddEntry extends StatefulWidget {
   const AddEntry({Key? key}) : super(key: key);
+
+  @override
+  _AddEntryState createState() => _AddEntryState();
+}
+
+class _AddEntryState extends State<AddEntry> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('New Journal Entry'),
-        actions: [
-          Builder(
-              builder: (context) => IconButton(
-                    icon: Icon(Icons.settings),
-                    onPressed: () => Scaffold.of(context).openEndDrawer(),
-                  ))
-        ],
-      ),
-      endDrawer: drawer(),
-      body: Form(child: TextFormField()),
-    );
+        appBar: AppBar(
+          leading: backButton(context),
+          centerTitle: true,
+          title: Text('New Journal Entry'),
+          actions: [
+            Builder(
+                builder: (context) => IconButton(
+                      icon: Icon(Icons.settings),
+                      onPressed: () => Scaffold.of(context).openEndDrawer(),
+                    ))
+          ],
+        ),
+        endDrawer: drawer(),
+        body: newEntryForm(context, _formKey));
   }
+}
+
+Widget backButton(BuildContext context) {
+  return Builder(builder: (BuildContext context) {
+    return IconButton(
+        onPressed: () => {Navigator.pop(context)},
+        icon: Icon(Icons.arrow_back));
+  });
+}
+
+Widget newEntryForm(BuildContext context, GlobalKey<FormState> formKey) {
+  return Form(
+      key: formKey,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Title', border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Body', border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            // DropdownRatingFormField(
+            //     maxRating: 4, validator: validator, onSaved: onSaved)
+            buttonRow(),
+          ],
+        ),
+      ));
+}
+
+// Row widget for Cancel and Save buttons
+Widget buttonRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      SizedBox(
+          width: 110,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.grey)),
+            onPressed: () {},
+            child: Text(
+              'Cancel',
+              style: TextStyle(fontSize: 18),
+            ),
+          )),
+      SizedBox(
+          width: 110,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Save',
+              style: TextStyle(fontSize: 18),
+            ),
+          )),
+    ],
+  );
 }
--- lib/src_export.dart
+++ lib/src_export.dart
@@ -3,3 +3,4 @@ export 'src/app.dart';
 export 'src/welcome_screen.dart';
 export 'src/drawer.dart';
 export 'src/add_entry.dart';
+export 'src/dropdown_rating_form_field.dart';
